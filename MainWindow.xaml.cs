54d62b5c33e59e46cd422fb31df3ebccb8f76eb8
Abobik <golenischev2109@mail.ru>
2024-01-28 22:34:19 +0300

    First Day

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AMOGUSIK
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public class PasswordToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string password && !string.IsNullOrEmpty(password))
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void enterClk(object sender, RoutedEventArgs e)
+        {
+            MainForm mf = new MainForm();
+            mf.Show();
+            this.Close();
+        }
+
+        private void RegClk(object sender, RoutedEventArgs e)
+        {
+            Registration Reg = new Registration();
+            Reg.Show();
+            this.Close();
+        }
+    }
+}
